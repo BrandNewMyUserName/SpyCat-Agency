1117e5a8e9923acde1700627a0d58657ac05111b
Shutko Illia <illasutko@gmail.com>
2025-10-18 16:29:35 +0300

    feat/spycat views

---
+++ agency/views.py
@@ -0,0 +1,45 @@
+from rest_framework import generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import SpyCat, Mission, Target
+from .serializers import (
+    SpyCatSerializer, 
+)
+
+
+class SpyCatListCreateView(generics.ListCreateAPIView):
+    queryset = SpyCat.objects.all()
+    serializer_class = SpyCatSerializer
+
+
+class SpyCatDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SpyCat.objects.all()
+    serializer_class = SpyCatSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+         
+        if instance.missions.filter(status__in=['pending', 'in_progress']).exists():
+            return Response(
+                {"error": "Cannot delete cat with active missions."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class SpyCatUpdateSalaryView(generics.UpdateAPIView):
+    queryset = SpyCat.objects.all()
+    serializer_class = SpyCatSerializer
+    
+    def patch(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+
+
