7a90d4aac40be29af368ad15e61173b804ad1590
Shutko Illia <illasutko@gmail.com>
2025-10-18 16:45:21 +0300

    feat/TargetInline and TargetAdmin

--- agency/admin.py
+++ agency/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SpyCat, Mission
+from .models import SpyCat, Mission, Target
 
 
 @admin.register(SpyCat)
@@ -10,10 +10,24 @@ class SpyCatAdmin(admin.ModelAdmin):
     ordering = ['name']
 
 
+class TargetInline(admin.TabularInline):
+    model = Target
+    extra = 0
+
+
 @admin.register(Mission)
 class MissionAdmin(admin.ModelAdmin):
     list_display = ['id', 'cat', 'status', 'created_at']
     list_filter = ['status', 'created_at']
     search_fields = ['cat__name']
+    inlines = [TargetInline]
     ordering = ['-created_at']
+
+
+@admin.register(Target)
+class TargetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'country', 'mission', 'is_completed', 'created_at']
+    list_filter = ['is_completed', 'country', 'created_at']
+    search_fields = ['name', 'country']
+    ordering = ['name']
 
